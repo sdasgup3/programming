9cb0b3202819dd0e19e1a097fddba9c1e918b3c0
Sandeep Dasgupta <sandigame.123@gmail.com>
2013-11-04 13:42:59 -0600

    test

---
+++ Parallel-Programming/MPI/Sample_Sort/Test/tester_sample.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+
+#my @sizes = (1200, 12000, 120000,  1200000, 12000000, 120000000);
+my @sizes = (1200, 12000, 120000,  1200000);
+my @cores = (2,6,12);
+
+
+foreach my $size (@sizes) {
+    foreach my $core (@cores) {
+        system("mpirun -n  $core ../psampleSort $size > out");
+
+print "Diffing psamplesort on size $size and core $core\t:";
+       my $res = `diff out  gold_$size`;
+       if("" eq $res) {
+           print "Pass \n";
+           system("rm -rf out");
+       } else {
+           print "Diff Exists\n";
+           exit(0);
+}
+    }
+}
