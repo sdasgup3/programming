bbfbe103322e2d57c451268edfd24b809fba7487
Sandeep Dasgupta <sdasgup3@illinois.edu>
2015-08-31 10:46:19 -0500

    used var

--- Scripting/Matlab/NBody/n_bodies_vec.m
+++ Scripting/Matlab/NBody/n_bodies_vec.m
@@ -1,5 +1,5 @@
 function   n_bodies
-    n=5000
+    n=50000
     clear pi;
     SOLAR_MASS=4*pi*pi;
     DAYS_PER_YEAR=365.24;
@@ -63,9 +63,9 @@ end
 
 function bodies_advance(bodies, nbodies, dt)
   
-  dSquared = 0.;
-  distance = 0.;
-  mag = 0.;
+  %dSquared = 0.;
+  %distance = 0.;
+  %mag = 0.;
 
   for ii=1:nbodies
     for jj=ii+1:nbodies
