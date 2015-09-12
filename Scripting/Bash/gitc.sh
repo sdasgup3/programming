77a156fde0f1801ebf4fc180c6f6d3ad028ac530
Sandeep Dasgupta <sdasgup3@illinois.edu>
2015-09-11 22:32:18 -0500

    "git commit script"

---
+++ Scripting/Bash/gitc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash  
+
+mod_files=`git status | grep modified | cut -f 2 -d : | xargs`;
+new_files=`git status | grep "new file" | cut -f 2 -d : | xargs`;
+file_to_commit="$mod_files $new_files" 
+
+echo $file_to_commit
+echo "Are U OK?"
+
+read R;
+if [ $R = "y" ] || [ $R = "Y" ] ; then
+  #This also works; the command line arg should be in quotes
+  #M="$1"; 
+  echo "Enter Message (with quotes)?";
+  read M
+  git pull && git commit -m "$M"  $file_to_commit && git push;
+fi
