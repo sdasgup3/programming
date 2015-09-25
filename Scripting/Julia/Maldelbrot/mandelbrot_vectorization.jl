f41a758608b32d1b718c04f43286890f0e7a02fd
Sandeep Dasgupta <sdasgup3@illinois.edu>
2015-09-25 14:48:02 -0500

    "timing updated"

--- Scripting/Julia/Maldelbrot/mandelbrot_vectorization.jl
+++ Scripting/Julia/Maldelbrot/mandelbrot_vectorization.jl
@@ -1,19 +1,3 @@
-type packedD
-  v2df::Array{Float64,1}
-end
-      
-function calc(bitmap)
-bitmap[1] = 5;
-bitmap[2] = 6;
-	end
-
-function XXX(N)
-    bitmap = zeros(2, 3);
-    calc(bitmap[1:2]);
-
-end
-
-
 function calc_row(y::Int64, N::Int64, bytes_per_row::Int64, 
 		Crv::Array{Float64,2}, inverse_h::Float64, bitmap,
 		zero::Array{Float64,2}, vec_four::Array{Float64,2},
@@ -24,11 +8,11 @@ function calc_row(y::Int64, N::Int64, bytes_per_row::Int64,
     
     
         
-    Civ = repmat(Civ_init, vec_size,1);
-    Zrv = repmat(zero, vec_size,1);
-    Ziv = repmat(zero, vec_size,1);
-    Trv = repmat(zero, vec_size,1);
-    Tiv = repmat(zero, vec_size,1);
+    Civ::Array{Float64,2} = repmat(Civ_init, vec_size,1);
+    Zrv::Array{Float64,2} = repmat(zero, vec_size,1);
+    Ziv::Array{Float64,2} = repmat(zero, vec_size,1);
+    Trv::Array{Float64,2} = repmat(zero, vec_size,1);
+    Tiv::Array{Float64,2} = repmat(zero, vec_size,1);
 
     for ii= 1:1:50 
         Ziv = (Zrv .* Ziv) + (Zrv .* Ziv) + Civ;
@@ -60,38 +44,27 @@ end
 function  mandelbrot_vectorization(N::Int64)
     println("N = " , N); 	
     bytes_per_row::Int64 = (N + 7) >>  3;
-#bytes_per_row::Int64 = (N + 7) >>  3;
 
     inverse_w = 2.0 / (bytes_per_row <<  3);
     inverse_h = 2.0 / N;
-#inverse_w::Float64 = 2.0 / (bytes_per_row <<  3);
-#    inverse_h::Float64 = 2.0 / N;
 
-    zero = Float64[];
-    four = Float64[];
     zero = [ 0.0 0.0 ];
     four = [ 4.0 4.0 ];
 
-    bitmap = zeros(UInt8, bytes_per_row, N);
-    vec_size = Int64(N/2); 
-    vec_size_for_reduction = Int64(N / (2*bytes_per_row)); 
-    vec_four = repmat(four,vec_size,1);
-#   bitmap::Array{UInt8,2} = zeros(UInt8, bytes_per_row, N);
-#    vec_size::Int64 = N/2; 
-#    vec_size_for_reduction::Int64 = N / (2*bytes_per_row); 
-#    vec_four::Array{Float64,2} = repmat(four,vec_size,1);
+    bitmap::Array{UInt8,2} = zeros(UInt8, bytes_per_row, N);
+    vec_size::Int64 = N/2; 
+    vec_size_for_reduction::Int64 = N / (2*bytes_per_row); 
+    vec_four::Array{Float64,2} = repmat(four,vec_size,1);
 
-#Crv::Array{Float64,2} = repmat([0.0 0.0], vec_size,1);
-#   X::Array{Int64,1} = [Int64(i) for i in 0:2:N-2];
-    Crv = repmat([0.0 0.0], vec_size,1);
-    X = [Int64(i) for i in 0:2:N-2];
+    Crv::Array{Float64,2} = repmat([0.0 0.0], vec_size,1);
+    X::Array{Int64,1} = [Int64(i) for i in 0:2:N-2];
 
     for ii::Int64 = 1:1:N/2 
 	Crv[ii] 		= ((2*ii-2) + 1.0)*inverse_w - 1.5;
 	Crv[ii + vec_size ] 	= (2*ii-2)*inverse_w - 1.5;
     end
     
-    for ii = 0:1:N-1
+    for ii::Int64 = 0:1:N-1
         calc_row(ii, N, bytes_per_row, Crv, inverse_h, bitmap, zero, vec_four, vec_size, vec_size_for_reduction,X);
     end
     
