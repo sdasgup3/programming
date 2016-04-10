c1e9122646ea3ca1432a7e1cc507b1d73c8828cf
Sandeep Dasgupta <sdasgup3@illinois.edu>
2016-04-10 03:05:23 -0500

    "commt"

--- scripting/bash/greater.sh
+++ scripting/bash/greater.sh
@@ -2,17 +2,17 @@
 
 read num1
 read num2
-if [ $num1 -gt $num2] ; then
-    echo "$num1 is greater than $num2"
+if [ $num1 -gt $num2 ] ; then
+    echo "$num1 is greater than $num2";
 elif [ $num1 -lt $num2 ]; then
-    echo "$num1 is lesser than $num2"
-else
-    echo "$num1 is equal to $num2"
+    echo "$num1 is lesser than $num2" ;
+else 
+    echo "$num1 is equal to $num2" ;
 fi  
 
 read C
 if [ $C = "y" ] || [ $C = "Y" ] ; then
-    echo YES
+    echo YES ; 
 elif   [ $C="n" ] || [ $C="N" ] ; then 
-    echo NO
+    echo NO ;
 fi
